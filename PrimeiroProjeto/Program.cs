1d71fa0bdf17866e335cb79d7749053e9e41f3ba
vpb <vitorpossebontra@gmail.com>
2025-02-21 12:17:56 -0300

    1

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Seja bem vindo ao começo do fim!!");
+            int n1, n2;
+            n1 = 5;
+            n2 = 25;
+            Console.WriteLine("n1 é igual a: "+n1);
+            Console.WriteLine("n2 é igual a: "+n2);
+            Console.WriteLine("A soma é igual a: "+(n1+n2));
+            int soma;
+            soma = n1 + n2;
+            Console.WriteLine("A variavel soma é igual a: "+soma);
         }
     }
 }
