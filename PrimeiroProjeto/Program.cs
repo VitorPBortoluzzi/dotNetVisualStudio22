0691e1b7314346885e36ecadf1527e9cb8279981
vpb <vitorpossebontra@gmail.com>
2025-02-21 12:05:17 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seja bem vindo ao começo do fim!!");
+        }
+    }
+}
