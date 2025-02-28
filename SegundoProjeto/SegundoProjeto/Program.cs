c87611ccede01c9ae4b595bb151613544815bb72
vpb <vitorpossebontra@gmail.com>
2025-02-28 12:33:25 -0300

    string

--- SegundoProjeto/SegundoProjeto/Program.cs
+++ SegundoProjeto/SegundoProjeto/Program.cs
@@ -21,7 +21,7 @@
 
             int i;
             i = 0;
-            Console.WriteLine("i = : "+i);
+            Console.WriteLine("i = : " + i);
             i = i + 1;
             Console.WriteLine("i = : " + i);
             i++;
@@ -32,6 +32,21 @@
             Console.WriteLine("i = : " + i);
             i = i + 3;
             Console.WriteLine("i = : " + i);
-        }   
+
+            Console.Write("Aqui eu n quebro linha!! ");
+            Console.Write("Aqui eu n quebro linha, mas me quebram a linha 2x!! \n \n");
+
+            Console.WriteLine("Manipulação de Strings: ");
+            string frase = "Frase de declaração de variavel string";
+            String frase2 = "Frase de declaração de variavel string2";
+
+            Console.WriteLine("Concatenando Strings");
+            Console.WriteLine("Frase 1 = " + frase);
+            Console.WriteLine("Frase 2= " + frase2 + " \n");
+
+            Console.WriteLine("Interpolando Strings");
+            Console.WriteLine($"frase 1 = {frase}");
+            Console.WriteLine($"frase 2 = {frase2}");
+        }
     }
 }
