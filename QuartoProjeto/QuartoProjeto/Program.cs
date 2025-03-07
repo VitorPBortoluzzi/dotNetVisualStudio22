cbaebc501b9b0572fc71734de1dc3c92a7757e54
VPB <vitorpossebontra@gmail.com>
2025-03-07 12:24:23 -0300

    QuartoProjeto

---
+++ QuartoProjeto/QuartoProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace QuartoProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Digite um número: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num == 0){ Console.WriteLine("O numero é 0");
+            }else if (num > 0) { Console.WriteLine("Positivo");
+                }else { Console.WriteLine("Negativo"); 
+                    }
+        }
+    }
+}
