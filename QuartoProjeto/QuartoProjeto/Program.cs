f50099ef3929a40a1ede348bb4b70cf09f8aefa1
VPB <vitorpossebontra@gmail.com>
2025-03-07 12:37:03 -0300

    Divisivil

--- QuartoProjeto/QuartoProjeto/Program.cs
+++ QuartoProjeto/QuartoProjeto/Program.cs
@@ -4,14 +4,26 @@
     {
         static void Main(string[] args)
         {
+            //int num;
+            //Console.WriteLine("Digite um número: ");
+            //num = int.Parse(Console.ReadLine());
+
+            //if (num == 0){ Console.WriteLine("O numero é 0");
+            //}else if (num > 0) { Console.WriteLine("Positivo");
+            //    }else { Console.WriteLine("Negativo"); 
+            //        }
+
+
+            //testar se um numero é divisivel por 2;
             int num;
-            Console.WriteLine("Digite um número: ");
+            Console.WriteLine("Digite um numero para verificar sua divisibilidade por 2: ");
             num = int.Parse(Console.ReadLine());
 
-            if (num == 0){ Console.WriteLine("O numero é 0");
-            }else if (num > 0) { Console.WriteLine("Positivo");
-                }else { Console.WriteLine("Negativo"); 
-                    }
+
+            if (num % 2 == 0) { Console.WriteLine("O numero " + num + " é divisivel por 2"); }
+            if (num % 4 == 0) { Console.WriteLine("O numero " + num + " é divisivel por 4"); }
+            if (num % 8 == 0) { Console.WriteLine("O numero " + num + " é divisivel por 8"); }
+
         }
     }
 }
