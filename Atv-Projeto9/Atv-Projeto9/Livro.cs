e07238329ae5c84ede1d7fdf84f2e4483b106a8b
VPb <vitorpossebontra@gmail.com>
2025-04-04 10:40:25 -0300

    Proj9

---
+++ Atv-Projeto9/Atv-Projeto9/Livro.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atv_Projeto9
+{
+    internal class Livro
+    {
+        //// 2- Crie uma classe Livro que represente os dados básicos de um livro,
+        // além destes, criar um atributo do tipo boolean chamado emprestado.
+        // Crie métodos emprestar e devolver que altera o atributo emprestado
+        // Crie um método construtor que receba todos os valores por parâmetros,
+        // exceto o atributo emprestado que obrigatoriamente deve ser inicializado como false
+        // Faça a leitura pelo teclado dos atributos para instanciar dois livros
+
+        public string titulo;
+        public string autor;
+        public string genero;
+        public int anoPublicacao;
+        public bool emprestado;
+
+        public Livro(string titulo, string autor, string genero, int anoPublicacao, bool emprestado = false)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.genero = genero;
+            this.anoPublicacao = anoPublicacao;
+            this.emprestado = emprestado;
+        }
+
+        public void emprestar()
+        {
+            if (emprestado != true)
+            {
+                emprestado = true;
+                Console.WriteLine("O livro está emprestado");
+            }
+            else
+            {
+                Console.WriteLine("o livro já está emprestado!!!");
+            }
+        }
+        public void devolver()
+        {
+            if (emprestado != false)
+            {
+                emprestado = false;
+                Console.WriteLine("O livro foi devolvido");
+            }
+            else
+            {
+                Console.WriteLine("o livro já foi devolvido!!!");
+            }
+        }
+}
