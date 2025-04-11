5fba55291d732a16a4b0bd614290232b5bafd4e3
VPB <vitorpossebontra@gmail.com>
2025-04-11 09:23:13 -0300

    10

---
+++ 10-ClassAbstrata/10-ClassAbstrata/Atv1/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_ClassAbstrata.Atv1
+{
+    internal class Aluno : Pessoa
+    {
+        public string Matricula;
+
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Sou o aluno {Nome}, tenho {Idade} anos e imnha matrícula é {Matricula}");
+        }
+    }
+}
---
+++ 10-ClassAbstrata/10-ClassAbstrata/Atv2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_ClassAbstrata.Atv2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro()
+            {
+                Marca = "Wolksvagem",
+                Modelo = "BMW",
+                Portas = 4
+
+            };
+
+            Moto moto = new Moto()
+            {
+                Marca = "Harley",
+                Modelo = "X",
+                Cilindradas = 250
+            };
+
+            moto.Dirigir();
+            carro.Dirigir();
+        }
+    }
+}
