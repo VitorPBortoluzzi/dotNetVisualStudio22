d330167225f2baaae2bc44f9e206d9035e1312f0
VPB <vitorpossebontra@gmail.com>
2025-04-11 08:46:31 -0300

    9-Personagem

---
+++ 9-Personagem/9-Personagem/Personagem.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Personagem
+{
+    internal class Personagem
+    {
+        public string Nome;
+        public int X = 0, Y = 0;
+        public List<string> Inventario;
+        public int HP;
+        public int Forca;
+        public int Inte;
+
+        public Personagem(string nome,int hp, int forca, int inte)
+        {
+            Nome = nome;
+            HP = hp;
+            Forca = forca;
+            Inte = inte;
+            X = 0;
+            Y = 0;
+            Inventario = new List<string>();
+        }
+
+        public void Mover(int direcao)
+        {
+            switch (direcao)
+            {
+                case 1: // frente
+                    Y += 1;
+                    break;
+                case 2: // tras
+                    Y -= 1;
+                    break;
+                case 3: // direita
+                    X+= 1;
+                    break;
+                case 4: // esquerda
+                    X -= 1;
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    return;
+            }
+            Console.WriteLine($"Nova posição: ({X},{Y})");
+        }
+
+        public void atacar(double dano) { 
+            if (dano < 0 || dano > 10)
+            {
+                Console.WriteLine("Dano inválido! Fora da escala de 0 a 10");
+                return;
+            }
+            Console.WriteLine($"{Nome} atacou causando {dano}/10 de dano.");
+        }
+
+        public void MostrarAtributos()
+        {
+            Console.WriteLine($"\n Atributos de {Nome}:");
+            Console.WriteLine($"HP: {HP}");
+            Console.WriteLine($"Força: {Forca}");
+            Console.WriteLine($"Intelecto: {Inte}");
+        }
+
+        public void MostrarIventario()
+        {
+            Console.WriteLine("\n Inventário:");
+            if (Inventario.Count == 0)
+            {
+                Console.WriteLine("Inventário vazio");
+            }
+            else
+            {
+                foreach (var item in Inventario)
+                {
+                    Console.WriteLine($"-{item}");
+                }
+            }
+        }
+
+        public void AdicionarItem(string item)
+        {
+            Inventario.Add(item);
+            Console.WriteLine($"{item} foi adicionado ao inventário.");
+        }
+
+        public void RemoverItem(string item)
+        {
+            if (Inventario.Contains(item))
+            {
+                Inventario.Remove(item);
+                Console.WriteLine($"{item} foi removido do inventário.");
+            }
+            else
+            {
+                Console.WriteLine("${item} não encontrado no inventário ");
+            }
+        }
+    }
+}
---
+++ 9-Personagem/9-Personagem/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _9_Personagem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome do seu personagem: ");
+            string nomePersonagem = Console.ReadLine();
+
+            Console.WriteLine("\n Defina os atributos do seu personagem: ");
+            Console.Write("HP: ");
+            int hp = int.Parse(Console.ReadLine());
+            Console.Write("Força: ");
+            int forca = int.Parse(Console.ReadLine());
+            Console.Write("Intelecto: ");
+            int intelecto = int.Parse(Console.ReadLine());
+
+            Personagem p = new Personagem(nomePersonagem,hp,forca,intelecto);
+            Console.WriteLine($"Personagem '{p.Nome}' criado na posição (0,0) com os seguintes atributos:");
+            p.MostrarAtributos();
+
+            while (true)
+            {
+                Console.WriteLine("\n Digite um comando (mover,atacar,inventario,pegar,largar,status,sair): ");
+                string comando = Console.ReadLine().ToLower();
+
+                if (comando == "sair") break;
+
+                switch (comando)
+                {
+                    case "mover":
+                        Console.Write("Digite a direção (1-frente,2-trás,3-direita,4-esquerda):");
+                        int direcao = int.Parse(Console.ReadLine());
+                        p.Mover(direcao);
+                        break;
+                    case "atacar":
+                        Console.WriteLine("Digite um valor para o dano entre 0 e 10: ");
+                        double dano = double.Parse(Console.ReadLine());
+                        p.atacar(dano);
+                        break;
+                    case "inventario":
+                        Console.WriteLine("Inventario: ");
+                        p.MostrarIventario();
+                        break;
+                    case "pegar":
+                        Console.WriteLine("Digite o nome do item que deseja pegar: ");
+                        string item = Console.ReadLine();
+                        p.AdicionarItem(item);
+                        break;
+                    case "largar":
+                        Console.WriteLine("Digite o nome do item que deseja largar: ");
+                        p.MostrarIventario();
+                        string item_remover = Console.ReadLine();
+                        p.RemoverItem(item_remover);
+                        break;
+                    case "status":
+                        Console.WriteLine("Atributos");
+                        p.MostrarAtributos();
+                        break;
+                }
+            }
+        }
+    }
+}
