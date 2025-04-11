03bc219e56eb9b748524db004c636d0a9c7afcc2
vpb <vitorpossebontra@gmail.com>
2025-04-11 12:20:00 -0300

    Prova1

---
+++ Prova/Prova/Chamadosoftware.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova
+{
+    internal class Chamadosoftware : Chamado
+    {
+        public Chamadosoftware(string Titulo, string Descricao) : base(Titulo, Descricao)
+        {
+            this.Titulo = Titulo;
+            this.Descricao = Descricao;
+            Executar();
+        }
+
+        public override void Executar()
+        {
+            Status = "Em Atendimento";
+            Console.WriteLine("Executando diagnóstico e resinstalação de software...");
+        }
+    }
+}
